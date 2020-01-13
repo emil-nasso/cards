018e1405aa082b6cfaf6b150c32247f4ba5c8227
Emil Andersson <nasso@nasso.se>
2020-01-13 23:46:18 +0100

    Extract bgStyle rendering to method

--- resources/js/components/Posts.vue
+++ resources/js/components/Posts.vue
@@ -5,7 +5,7 @@
             <div class="flex flex-wrap -mx-2">
                 <div class="mb-4 w-full px-2 lg:w-1/2 lg:flex" v-for="post in category.posts" v-bind:key="post.id">
                     <div v-if="post.image" class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
-                        v-bind:style="'background-image: url(\'' + post.image.custom_properties.url + '\')'"/>
+                        v-bind:style="bgStyle(post)"/>
                     <div v-bind:class="{ 'lg:border-l-0 lg:rounded-b-none' : post.image, 'border-t rounded': !post.image }" class="w-full border-r border-b border-l border-gray-400 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-r p-4 flex flex-col justify-between leading-normal">
                         <div class="mb-8">
                             <div class="text-gray-900 font-bold text-xl mb-2">{{ post.title }}</div>
@@ -33,6 +33,11 @@
         },
         mounted() {
             console.log(this.categories);
+        },
+        methods: {
+            bgStyle: function(post) {
+                return 'background-image: url(\'' + post.image.custom_properties.url + '\')';
+            }
         }
     }
 </script>
