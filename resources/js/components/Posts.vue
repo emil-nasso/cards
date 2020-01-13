5a0a13d55c90fab08a8f80fe92c561c47daa31f4
Emil Andersson <nasso@nasso.se>
2020-01-13 23:24:15 +0100

    Set full with for post body

--- resources/js/components/Posts.vue
+++ resources/js/components/Posts.vue
@@ -7,7 +7,7 @@
                     <div class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
                         style="background-image: url('https://via.placeholder.com/150')"/>
                     <!-- <img class="rounded" v-if="post.image" :src="post.image.custom_properties.url"/> -->
-                    <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+                    <div class="w-full border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
                         <div class="mb-8">
                             <div class="text-gray-900 font-bold text-xl mb-2">{{ post.title }}</div>
                             <p class="text-gray-700 text-base" v-html="post.body"></p>
